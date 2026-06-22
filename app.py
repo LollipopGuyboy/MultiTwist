4798efebe31c12bf79b8f316b636f40caaf0bb9f
LollipopGuyboy <rishabhgoodboy999@gmail.com>
2026-06-22 13:53:11 +0530

    Serve multitwist.html instead of text response

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "MultiTwist is alive!"
-    
+    return send_file("multitwist.html")
+
 if __name__ == "__main__":
     app.run()
